606b340e22d7dff9e82bffe1bb8b774025f09ba6
Sajan Shrestha <s.shrestha@fetchrewards.com>
2021-06-26 13:11:35 -0400

    swift ui views project started

---
+++ SwiftUI Views/SwiftUI_ViewsApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_ViewsApp.swift
+//  SwiftUI Views
+//
+//  Created by Sajan Shrestha on 6/26/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_ViewsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

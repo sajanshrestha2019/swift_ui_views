efc9c285cd23b711a7d66f4ebb6a7d530f3969ae
Sajan Shrestha <s.shrestha@fetchrewards.com>
2021-06-27 16:04:47 -0400

    testing git push

--- SwiftUI Views/ContentView.swift
+++ SwiftUI Views/ContentView.swift
@@ -8,9 +8,29 @@
 import SwiftUI
 
 struct ContentView: View {
+    let images = [
+        "https://cdn.pixabay.com/photo/2021/06/20/16/57/woman-6351539_1280.jpg",
+        "https://cdn.pixabay.com/photo/2020/10/14/19/49/santorini-5655299_1280.jpg",
+        "https://cdn.pixabay.com/photo/2021/05/28/11/39/girl-6290663__480.jpg",
+        "https://cdn.pixabay.com/photo/2021/06/16/21/46/parrot-6342271__480.jpg"
+    ]
+
+    let columns: [GridItem] = Array(repeating: .init(.flexible()), count: 2)
+
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        ScrollView {
+            LazyVGrid(columns: columns) {
+                ForEach(images, id: \.self) { image in
+                    AsyncImage(url: URL(string: image), scale: 1.0) { image in
+                        image
+                            .resizable()
+                            .scaledToFill()
+                    } placeholder: {
+                        Image(systemName: "cloud")
+                    }
+                }
+            }
+        }
     }
 }
 
@@ -18,4 +38,10 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
     }
+}
+
+struct Person: Identifiable {
+    var name: String
+    var profession: String
+    var id = UUID()
 }
